68d692daa626b33bbfba0092374a2dd0af075c95
georgmagne <georgmagne@gmail.com>
2019-09-24 14:27:08 +0200

    Ferdig med progging, noen tekstoppgaver gjennstår.

--- 1oblig/MAC-prekode.vhd
+++ 1oblig/MAC-prekode.vhd
@@ -8,7 +8,7 @@ library IEEE;
 entity MAC is
     generic (width: integer := 8);
     port(
-        clk, reset  : in STD_LOGIC; -- Første feil, mangler semikolon
+        clk, reset  : in STD_LOGIC;
         MLS_select  : in STD_LOGIC;
         Rn, Rm, Ra  : in STD_LOGIC_VECTOR(width-1 downto 0);
         Rd          : out STD_LOGIC_VECTOR(width-1 downto 0)
@@ -16,32 +16,41 @@ entity MAC is
     end;
 
 architecture behavioral of MAC is
-    signal buffMLS                    : STD_LOGIC;
+    signal buffMLS, buffMLS2                    : STD_LOGIC;
     signal mul1, mul2, add1, buffRa 	: UNSIGNED(width-1 downto 0);
-    signal add2, sum, sub		        : UNSIGNED(width*2-1 downto 0);
-    signal buffProd 		                : UNSIGNED(width*2-1 downto 0);
+    signal add2, sum, sub             : UNSIGNED(width*2-1 downto 0);
+    signal buffProd, buffSum, buffSub : UNSIGNED(width*2-1 downto 0);
 
     begin
-      process(clk, reset, MLS_select)
+      process(clk, reset)
 
       begin
         if reset = '1' then
-          Rd <= (others => '0'); -- asynkron reset.
+          Rd <= (others => '0');          -- asynkron reset.
           buffProd <= (others => '0');
           buffRa <= (others => '0');
+          buffMLS <= '0';
+          buffSum <= (others => '0');
+          buffSub <= (others => '0');
 
-      elsif rising_edge(clk) then                     -- Skjer på klokkeflanken.
-        buffProd <= UNSIGNED(mul1*mul2);
-        buffRa <= UNSIGNED(Ra);
 
+      elsif rising_edge(clk) then          -- Skjer på klokkeflanken.
+        buffRa <= add1;
+        buffProd <= add2;
+
+        buffMLS2 <= buffMLS;
         buffMLS <= MLS_select;
 
-        if buffMLS = '1' then
-          Rd <= STD_LOGIC_VECTOR(sub(width-1 downto 0));
+        buffSum <= sum;
+        buffSub <= sub;
 
-        elsif buffMLS = '1' then
-          Rd <= STD_LOGIC_VECTOR(sum(width-1 downto 0)); -- Ta vare på LSB. -- Andre feil, sum var sm.
+        if buffMLS2 = '1' then
+          Rd <= STD_LOGIC_VECTOR(buffSub(width-1 downto 0));
+
+        elsif buffMLS2 = '0' then
+          Rd <= STD_LOGIC_VECTOR(buffSum(width-1 downto 0)); -- Ta vare på LSB. -- Andre feil, sum var sm.
         end if;
+
       end if;
 
     end process;
@@ -51,9 +60,9 @@ architecture behavioral of MAC is
 
     mul1 <= UNSIGNED(Rn);
     mul2 <= UNSIGNED(Rm);
-    add1 <= buffRa; -- Endrer add1 til å være den buffrete verdien til ra
-    add2 <= buffProd; -- Endrer add2 til å være den buffrete verdien til produktet Rn*Rm
-    sum <= add1 + add2;
-    sub <= add1 - add2;
+    add1 <= UNSIGNED(Ra);
+    add2 <= mul1*mul2;
+    sum <= buffRa + buffProd; -- Endrer sum til å bruke bufferene ikke inputene direkte.
+    sub <= buffRa - buffProd; -- Endrer sub til å bruke bufferene ikke inputene direkte.
 
   end architecture;
